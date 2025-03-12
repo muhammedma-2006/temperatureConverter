d7cae71af2e39188de61b628510caee93391e778
Muhammed M A <muhammedmaponnani@gmail.com>
2025-03-12 13:45:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const text = document.getElementById("textbox");
+const toFahrenheit = document.getElementById("toFahrenheit");
+const toCelsius = document.getElementById("toCelsius");
+const result = document.getElementById("result");
+let temp;
+
+function convert(){
+    
+    if(toFahrenheit.checked){
+        temp = Number(text.value);
+        temp= temp*(9/5)+32;
+        result.textContent = temp.toFixed(1)+"°F";
+    }
+    else if(toCelsius.checked){
+        temp = Number(text.value);
+        temp= (temp-32)*(5/9);
+        result.textContent = temp.toFixed(1) + "°C";
+    }
+    else{
+        result.textContent = "select a unit";
+    }
+
+}
